52dfb2b92888bb37c84bf835be9551a39c151cfb
qngvin <quangvinh11602@gmail.com>
2024-06-17 03:26:31 +0700

    analycis

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './App.css'
+import { Analytics } from '@vercel/analytics/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <Analytics />
     <App />
   </React.StrictMode>,
 )
